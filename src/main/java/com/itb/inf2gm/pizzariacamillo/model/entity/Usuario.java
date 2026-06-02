d9f68908d9ae7b9ed34eb4517752ae792adfb31b
Pablo-numsei <augustopablo620@gmail.com>
2026-06-02 12:29:29 -0300

    Camada Model Entity em construção

---
+++ src/main/java/com/itb/inf2gm/pizzariacamillo/model/entity/Usuario.java
@@ -0,0 +1,23 @@
+package com.itb.inf2gm.pizzariacamillo.model.entity;
+
+
+import com.itb.inf2gm.pizzariacamillo.model.enums.TipoUsuario;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+    private String sexo;
+    private String logradouro;
+    private String cep;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private boolean codStatus;
+    private TipoUsuario tipoUsuario;
+
+}
+
---
+++ src/main/java/com/itb/inf2gm/pizzariacamillo/model/enums/TipoUsuario.java
@@ -0,0 +1,11 @@
+package com.itb.inf2gm.pizzariacamillo.model.enums;
+
+public enum TipoUsuario {
+
+
+    ADMIN,
+    CLIENTE,
+    FUNCIONARIO,
+
+
+}
